ab2cfcf0ceb08de0d5117ca6efc2bc74465b4c93
Oleg <24privat@users.noreply.github.com>
2013-11-15 09:27:18 +0200

    Create persona_kod1c_labor.php

---
+++ persona_kod1c_labor.php
@@ -0,0 +1,32 @@
+<?php include "../../sess.inc";
+
+
+$user_id = @$_SESSION['auth']['uid'];
+$text=trim($_POST['name_labor']);
+$acc=Array();
+
+
+
+
+
+$sel="SELECT inn, `name`
+      FROM `spr_personal` WHERE `name` LIKE '%".$_POST['name_labor']."%' LIMIT 1";// . $sWhere;
+
+//echo $sel;exit;
+
+
+$acc=$DB->select($sel);
+
+
+ $answ='';
+
+ foreach($acc as $val){
+	 $answ.='<div style="height:20px;" id="'.$val['inn'].'" onclick="selname(this)" onMouseOver=newtextdiv(this) onMouseOut=backtextdiv(this)>'.$val['name'].'</div>';
+ }
+ echo $answ;
+
+
+
+
+?>
+
